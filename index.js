366293d2e22f9e50e8bd638ed1b9b77657b1a6f7
VerxMaxum <138268061+VerxMaxum@users.noreply.github.com>
2024-06-01 20:52:05 +0800

    Fix functionality to restart to X after 1 round

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 let turn = 0;
-let board = ['', '', '', '', '', '', '', '', '', ];
-let player ;
+let board = ['', '', '', '', '', '', '', '', ''];
+let player;
 let currPlayer;
+let winningGames = 0;
 
 const buttonSymbols = Array.from(document.getElementsByClassName('button-symbol'));
 const slots = Array.from(document.getElementsByClassName('slot'));
@@ -19,10 +20,13 @@ function initialize() {
         button.disabled = true;
         button.addEventListener("click", setSymbol);
     });
+    
+    restartButton.addEventListener("click", restart);
 }
 
 function startGame(event) {
     player = makePlayer(event);
+    console.log(player);
     dialog.close();
     slots.forEach((button) => {
         button.disabled = false;
@@ -55,7 +59,7 @@ function makePlayer(event) {
 
     name = nameInput.value;
 
-    return {name:name, symbol:symbol, turn:turn};
+    return {name:name, symbol:symbol, turn:turn, win:0};
 }
 
 function setSymbol(event) {
@@ -63,8 +67,9 @@ function setSymbol(event) {
 
     button.textContent = currPlayer;
     button.disabled = true;
-
-    turn++
+    board[button.value] = currPlayer;
+    console.log(board);
+    turn++;
     if(turn % 2 === player.turn)
         currPlayer = player.symbol;
     else if(player.symbol === 'X') {
@@ -73,6 +78,103 @@ function setSymbol(event) {
     else {
         currPlayer = 'X';
     }
+    checkWinner();
+}
+
+function checkWinner() {
+    const win = [
+        [0,1,2],
+        [3,4,5],
+        [6,7,8],
+        [0,3,6],
+        [1,4,7],
+        [2,5,8],
+        [0,4,8],
+        [2,4,6]
+    ]
+    console.log(player.symbol);
+
+    win.forEach((w) => {
+        if(board[w[0]] === board[w[1]] &&
+           board[w[1]] === board[w[2]]) {
+            if(board[w[0]] === player.symbol) {
+                winningGames++;
+                player.win++;
+                console.log("You won this round")
+                clearGame();
+            } else if(board[w[0]] !== '') {
+                console.log("Your foe won this round");
+                winningGames++;
+                clearGame();
+            }
+        }
+        else if(!board.includes('')) {
+            console.log("It's a tie in this round");
+            clearGame();
+        }
+    });
+
+    console.log(`Winning games: ${winningGames}`);
+
+    if(winningGames >= 3 && winningGames - player.win === 3) {
+        console.log("You lose");
+        restart();
+    } else if(winningGames >= 3 && player.win === 3){
+        console.log("You won!");
+        restart();
+    }
+}
+
+function clearGame() {
+    console.log(turn);
+    console.log(player);
+    turn = 0;
+    console.log(turn);
+    slots.forEach((button) => {
+        button.textContent = "";
+        button.disabled = false;
+    });
+    for(let i = 0; i < 9; i++) {
+        board[i] = "";
+    }
+
+    if(turn % 2 === player.turn)
+        currPlayer = player.symbol;
+    else if(player.symbol === 'X') {
+        currPlayer = 'O';
+    }
+    else {
+        currPlayer = 'X';
+    }
+}
+
+function restart() {
+    slots.forEach((s) => {
+        s.disabled = true;
+        s.textContent = '';
+    });
+    buttonSymbols.forEach((b) => {
+        b.disabled = false;
+    });
+    for(let i = 0; i < board.length; i++) {
+        board[i] = "";
+    }
+
+    if(turn % 2 === player.turn)
+        currPlayer = player.symbol;
+    else if(player.symbol === 'X') {
+        currPlayer = 'O';
+    }
+    else {
+        currPlayer = 'X';
+    }
+
+    currPlayer = null;
+    player = null;
+    turn = 0;
+    winningGames = 0;
+    console.log(board);
+    dialog.showModal();
 }
 
 initialize();
